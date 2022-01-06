745736a2e5b71eca30212aebd6f5905dc2065c6e
Marcus Ortiz <marcus_ortiz@apple.com>
2022-01-06 11:51:11 -0800

    Document `isParentSymbolKind`.

--- src/utils/symbols.js
+++ src/utils/symbols.js
@@ -1,5 +1,12 @@
 import SymbolKind from 'docc-render/constants/SymbolKind';
 
+// Returns whether or not the given symbol kind can be considered a potential
+// "parent" of other symbols.
+//
+// Classes, enums, protocols, and structs can be thought of as parent symbols
+// that may contain child symbol members for any of its
+// properties/functions/initializers/etc for example.
+//
 // eslint-disable-next-line import/prefer-default-export
 export function isParentSymbolKind(kind) {
   switch (kind) {
