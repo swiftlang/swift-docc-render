043c348b490336fcdfbf506c005adbcc4d21f3ce
Marcus Ortiz <marcus_ortiz@apple.com>
2022-01-06 13:48:48 -0800

    Add license header copy.

--- src/utils/symbols.js
+++ src/utils/symbols.js
@@ -1,3 +1,12 @@
+/**
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
 import SymbolKind from 'docc-render/constants/SymbolKind';
 
 // Returns whether or not the given symbol kind can be considered a potential
--- tests/unit/utils/symbols.spec.js
+++ tests/unit/utils/symbols.spec.js
@@ -1,3 +1,12 @@
+/**
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
 import { isParentSymbolKind } from 'docc-render/utils/symbols';
 import SymbolKind from 'docc-render/constants/SymbolKind';
 
