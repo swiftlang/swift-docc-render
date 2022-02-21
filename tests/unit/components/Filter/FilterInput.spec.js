bf2cdb9ee675fa8a83dbdb47eeb8a503c62f5590
Marina Aísa <marina.aisa@apple.com>
2022-02-21 20:27:58 +0100

    chore: update license header year

--- tests/unit/components/Filter/FilterInput.spec.js
+++ tests/unit/components/Filter/FilterInput.spec.js
@@ -1,3 +1,13 @@
+/**
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2022 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
 import { prepareDataForHTMLClipboard } from '@/utils/clipboard';
 import { shallowMount } from '@vue/test-utils';
 import FilterInput from '@/components/Filter/FilterInput.vue';
@@ -25,7 +35,6 @@ jest.mock('@/utils/input-helper', () => ({
 
 const {
   SuggestedTagsId,
-  SelectedTagsId,
   FilterInputId,
 } = FilterInput.constants;
 
