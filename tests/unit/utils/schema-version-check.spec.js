7198f89360f2c6e1e369e1f60b8d9cd651ae8f52
Dobromir Hristov <dhristov@apple.com>
2022-03-10 15:07:57 +0200

    chore: eslint fix

--- tests/unit/utils/schema-version-check.spec.js
+++ tests/unit/utils/schema-version-check.spec.js
@@ -6,7 +6,7 @@
  *
  * See https://swift.org/LICENSE.txt for license information
  * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
- */
+*/
 
 import emitWarningForSchemaVersionMismatch, {
   CURRENT_SUPPORTED_SCHEMA,
