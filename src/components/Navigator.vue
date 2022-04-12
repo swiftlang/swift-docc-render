9b6f5c3e84456d541e90cca264e99023bcb07472
Dobromir Hristov <dhristov@apple.com>
2022-04-12 10:02:08 +0300

    fix: remove extra borders from Navigator

--- src/components/Navigator.vue
+++ src/components/Navigator.vue
@@ -187,7 +187,6 @@ export default {
 
 .navigator {
   height: 100%;
-  border-left: 1px solid var(--color-grid);
   display: flex;
   flex-flow: column;
 
@@ -197,7 +196,6 @@ export default {
 
   @include breakpoint(medium, nav) {
     position: static;
-    border-left: none;
     transition: none;
   }
 }
