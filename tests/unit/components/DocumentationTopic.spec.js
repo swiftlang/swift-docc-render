swiftlang/swift-docc-render

commit f12b885045341728ebf94329f0581747a933753e

Add Hero and Content Style Changes  (#113)
Fixes:
rdar://89092590
rdar://89049211
rdar://86725278

Co-authored-by: Dobromir Hristov <dhristov@apple.com>
Co-authored-by: Marcus Ortiz <marcus_ortiz@apple.com>
Co-authored-by: Hanqing Huang <hqh@apple.com>

src/components/DocumentationTopic.vue
@@ -1,7 +1,7 @@
 <!--
   This source file is part of the Swift.org open source project
 
-  Copyright (c) 2021 Apple Inc. and the Swift project authors
+  Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
   Licensed under Apache License v2.0 with Runtime Library Exception
 
   See https://swift.org/LICENSE.txt for license information
@@ -11,11 +11,30 @@
 <template>
   <div class="doc-topic">
     <main class="main" id="main" role="main" tabindex="0">
-      <slot name="above-title" />
-      <Title :eyebrow="roleHeading">{{ title }}</Title>
-      <div class="container content-grid" :class="{ 'full-width': hideSummary }">
-        <Description :hasOverview="hasOverview">
-          <Abstract v-if="abstract" :content="abstract" />
+      <DocumentationHero :type="symbolKind || role" :enhanceBackground="enhanceBackground">
+        <slot name="above-title" />
+        <LanguageSwitcher
+          v-if="shouldShowLanguageSwitcher"
+          :interfaceLanguage="interfaceLanguage"
+          :objcPath="objcPath"
+          :swiftPath="swiftPath"
+        />
+        <Title :eyebrow="roleHeading">
+          {{ title }}
+          <small v-if="isSymbolDeprecated" slot="after" class="deprecated">Deprecated</small>
+          <small v-else-if="isSymbolBeta" slot="after" class="beta">Beta</small>
+        </Title>
+        <Abstract v-if="abstract" :content="abstract" />
+        <div v-if="sampleCodeDownload">
+          <DownloadButton class="sample-download" :action="sampleCodeDownload.action" />
+        </div>
+        <Availability
+          v-if="hasAvailability"
+          :platforms="platforms" :technologies="technologies"
+        />
+      </DocumentationHero>
+      <div v-if="showContainer" class="container">
+        <div class="description">
           <RequirementMetadata
             v-if="isRequirement"
             :defaultImplementationsCount="defaultImplementationsCount"
@@ -29,27 +48,10 @@
           >
             <ContentNode :content="downloadNotAvailableSummary" />
           </Aside>
-          <DownloadButton v-if="sampleCodeDownload" :action="sampleCodeDownload.action" />
-        </Description>
-        <Summary v-if="!hideSummary">
-          <LanguageSwitcher
-            v-if="shouldShowLanguageSwitcher"
-            :interfaceLanguage="interfaceLanguage"
-            :objcPath="objcPath"
-            :swiftPath="swiftPath"
-          />
-          <Availability v-if="platforms" :platforms="platforms" />
-          <TechnologyList v-if="modules" :technologies="modules" />
-          <TechnologyList
-            v-if="extendsTechnology"
-            class="extends-technology"
-            title="Extends"
-            :technologies="[{ name: extendsTechnology }]"
-          />
-          <OnThisPageNav v-if="onThisPageSections.length > 1" :sections="onThisPageSections" />
-        </Summary>
+        </div>
         <PrimaryContent
           v-if="primaryContentSections && primaryContentSections.length"
+          :class="{ 'with-border': !enhanceBackground }"
           :conformance="conformance"
           :sections="primaryContentSections"
         />
@@ -81,24 +83,20 @@
 <script>
 import Language from 'docc-render/constants/Language';
 import metadata from 'docc-render/mixins/metadata';
-import { getSetting } from 'docc-render/utils/theme-settings';
 
 import Aside from 'docc-render/components/ContentNode/Aside.vue';
 import BetaLegalText from 'theme/components/DocumentationTopic/BetaLegalText.vue';
 import LanguageSwitcher from 'theme/components/DocumentationTopic/Summary/LanguageSwitcher.vue';
+import DocumentationHero from 'docc-render/components/DocumentationTopic/DocumentationHero.vue';
 import Abstract from './DocumentationTopic/Description/Abstract.vue';
 import ContentNode from './DocumentationTopic/ContentNode.vue';
 import CallToActionButton from './CallToActionButton.vue';
 import DefaultImplementations from './DocumentationTopic/DefaultImplementations.vue';
-import Description from './DocumentationTopic/Description.vue';
-import TechnologyList from './DocumentationTopic/Summary/TechnologyList.vue';
-import OnThisPageNav from './DocumentationTopic/Summary/OnThisPageNav.vue';
 import PrimaryContent from './DocumentationTopic/PrimaryContent.vue';
 import Relationships from './DocumentationTopic/Relationships.vue';
 import RequirementMetadata from './DocumentationTopic/Description/RequirementMetadata.vue';
 import Availability from './DocumentationTopic/Summary/Availability.vue';
 import SeeAlso from './DocumentationTopic/SeeAlso.vue';
-import Summary from './DocumentationTopic/Summary.vue';
 import Title from './DocumentationTopic/Title.vue';
 import Topics from './DocumentationTopic/Topics.vue';
 
@@ -121,22 +119,19 @@ export default {
     },
   },
   components: {
+    DocumentationHero,
     Abstract,
     Aside,
     BetaLegalText,
     ContentNode,
     DefaultImplementations,
-    Description,
     DownloadButton: CallToActionButton,
-    TechnologyList,
     LanguageSwitcher,
-    OnThisPageNav,
     PrimaryContent,
     Relationships,
     RequirementMetadata,
     Availability,
     SeeAlso,
-    Summary,
     Title,
     Topics,
   },
@@ -225,9 +220,6 @@ export default {
       type: Object,
       default: () => ({}),
     },
-    extendsTechnology: {
-      type: String,
-    },
     tags: {
       type: Array,
       required: true,
@@ -280,12 +272,12 @@ export default {
         0,
       );
     },
-    hasOverview: ({ primaryContentSections = [] }) => primaryContentSections.filter(section => (
-      section.kind === PrimaryContent.constants.SectionKind.content
-    )).length > 0,
     onThisPageSections() {
       return this.topicState.onThisPageSections;
     },
+    hasAvailability: ({ platforms, technologies }) => (
+      (platforms || []).length || (technologies || []).length
+    ),
     hasBetaContent:
       ({ platforms }) => platforms
         && platforms.length
@@ -294,8 +286,31 @@ export default {
     pageDescription: ({ abstract, extractFirstParagraphText }) => (
       abstract ? extractFirstParagraphText(abstract) : null
     ),
-    shouldShowLanguageSwitcher: ({ objcPath, swiftPath }) => objcPath && swiftPath,
-    hideSummary: () => getSetting(['features', 'docs', 'summary', 'hide'], false),
+    shouldShowLanguageSwitcher: ({ objcPath, swiftPath, isTargetIDE }) => (
+      objcPath && swiftPath && isTargetIDE
+    ),
+    enhanceBackground: ({ symbolKind }) => (symbolKind ? (symbolKind === 'module') : true),
+    technologies({ modules = [] }) {
+      const technologyList = modules.reduce((list, module) => {
+        list.push(module.name);
+        return list.concat(module.relatedModules || []);
+      }, []);
+      // only show badges for technologies when there are multiple
+      return technologyList.length > 1
+        ? technologyList
+        : [];
+    },
+    showContainer: ({
+      isRequirement,
+      deprecationSummary,
+      downloadNotAvailableSummary,
+      primaryContentSections,
+    }) => (
+      isRequirement
+      || (deprecationSummary && deprecationSummary.length)
+      || (downloadNotAvailableSummary && downloadNotAvailableSummary.length)
+      || (primaryContentSections && primaryContentSections.length)
+    ),
   },
   methods: {
     normalizePath(path) {
@@ -361,44 +376,35 @@ export default {
 }
 
 .container {
-  margin-top: $section-spacing-single-side / 2;
   outline-style: none;
   @include dynamic-content-container;
 }
 
-.content-grid {
-  display: grid;
-  grid-template-columns: 75% 25%;
-  grid-template-rows: auto minmax(0, 1fr);
-
-  @include breakpoint(small) {
-    display: block;
-  }
-
-  &:before, &:after {
+.description {
+  &:empty {
     display: none;
   }
 
-  &.full-width {
-    grid-template-columns: 100%;
+  &:not(:empty) {
+    margin-bottom: $contenttable-spacing-single-side;
+  }
+
+  /deep/ .content + * {
+    margin-top: $stacked-margin-large;
   }
 }
 
-.description {
-  grid-column: 1;
+// remove border-top for first section of the page
+/deep/ {
+  .documentation-hero + .contenttable {
+    .container > .title {
+      border-top: none;
+    }
+  }
 }
 
-.summary {
-  grid-column: 2;
-  grid-row: 1 / -1;
-}
-
-.primary-content {
-  grid-column: 1;
-}
-
-.button-cta {
-  margin-top: 2em;
+.sample-download {
+  margin-top: 20px;
 }
 
 /deep/ {

src/components/DocumentationTopic/Title.vue
@@ -1,7 +1,7 @@
 <!--
   This source file is part of the Swift.org open source project
 
-  Copyright (c) 2021 Apple Inc. and the Swift project authors
+  Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
   Licensed under Apache License v2.0 with Runtime Library Exception
 
   See https://swift.org/LICENSE.txt for license information
@@ -11,7 +11,10 @@
 <template>
   <div class="topictitle">
     <span v-if="eyebrow" class="eyebrow">{{eyebrow}}</span>
-    <WordBreak class="title" tag="h1"><slot /></WordBreak>
+    <WordBreak class="title" tag="h1">
+      <slot />
+      <slot name="after" />
+    </WordBreak>
   </div>
 </template>
 
@@ -33,19 +36,44 @@ export default {
 <style scoped lang="scss">
 @import 'docc-render/styles/_core.scss';
 
-.topictitle {
-  @include dynamic-content-container;
-  margin-top: 2rem;
-}
-
 .eyebrow {
   @include font-styles(eyebrow-reduced);
-  color: var(--colors-secondary-label, var(--color-secondary-label));
+  color: dark-color(figure-gray-secondary);
   display: block;
   margin-bottom: rem(20px);
+
+  .documentation-hero--disabled & {
+    color: var(--colors-secondary-label, var(--color-secondary-label));
+  }
 }
 
 .title {
   @include font-styles(headline-reduced);
+  color: dark-color(figure-gray);
+  margin-bottom: rem(12px);
+
+  .documentation-hero--disabled & {
+    color: var(--colors-header-text, var(--color-header-text));
+  }
+}
+
+small {
+  @include font-styles(eyebrow);
+
+  &.beta {
+    color: var(--color-badge-beta);
+
+    .theme-dark & {
+      color: var(--color-badge-dark-beta);
+    }
+  }
+
+  &.deprecated {
+    color: var(--color-badge-deprecated);
+
+    .theme-dark & {
+      color: var(--color-badge-dark-deprecated);
+    }
+  }
 }
 </style>

tests/unit/components/DocumentationTopic.spec.js
@@ -1,7 +1,7 @@
 /**
  * This source file is part of the Swift.org open source project
  *
- * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
  * Licensed under Apache License v2.0 with Runtime Library Exception
  *
  * See https://swift.org/LICENSE.txt for license information
@@ -9,31 +9,23 @@
 */
 
 import { shallowMount } from '@vue/test-utils';
-import { getSetting } from 'docc-render/utils/theme-settings';
 import DocumentationTopic from 'docc-render/components/DocumentationTopic.vue';
 import Language from 'docc-render/constants/Language';
 import { TopicTypes } from '@/constants/TopicTypes';
-
-jest.mock('docc-render/utils/theme-settings', () => ({
-  getSetting: jest.fn((_, fallback) => fallback),
-}));
+import DocumentationHero from '@/components/DocumentationTopic/DocumentationHero.vue';
 
 const {
   Abstract,
   ContentNode,
   DefaultImplementations,
   Aside,
-  Description,
   DownloadButton,
-  TechnologyList,
   LanguageSwitcher,
-  OnThisPageNav,
   PrimaryContent,
   Relationships,
   RequirementMetadata,
   Availability,
   SeeAlso,
-  Summary,
   Topics,
   Title,
   BetaLegalText,
@@ -107,7 +99,7 @@ const sampleCodeDownload = {
 
 const propsData = {
   abstract: [abstract],
-  conformance: { constraints: [], availabilityPrefx: [] },
+  conformance: { constraints: [], availabilityPrefix: [] },
   hierarchy: {
     paths: [
       [
@@ -121,6 +113,18 @@ const propsData = {
   symbolKind: TopicTypes.module,
   objcPath: 'documentation/objc',
   swiftPath: 'documentation/swift',
+  technology: { title: 'fookit' },
+  platforms: [
+    {
+      introducedAt: '1.0',
+      name: 'fooOS',
+    },
+    {
+      deprecatedAt: '2.0',
+      introducedAt: '1.0',
+      name: 'barOS',
+    },
+  ],
   primaryContentSections: [
     {
       kind: PrimaryContent.constants.SectionKind.content,
@@ -204,16 +208,89 @@ describe('DocumentationTopic', () => {
     expect(main.attributes('tabindex')).toBe('0');
   });
 
+  it('renders a `DocumentationHero`, enabled', () => {
+    const hero = wrapper.find(DocumentationHero);
+    expect(hero.exists()).toBe(true);
+    expect(hero.props()).toEqual({ type: propsData.symbolKind, enhanceBackground: true });
+  });
+
+  it('renders a `DocumentationHero`, enabled, with a the `role`, if no symbolKind', () => {
+    wrapper.setProps({
+      role: TopicTypes.article,
+      symbolKind: '',
+    });
+    const hero = wrapper.find(DocumentationHero);
+    expect(hero.props()).toEqual({ type: TopicTypes.article, enhanceBackground: true });
+  });
+
+  it('render a `DocumentationHero`, enabled, if top-level technology page', () => {
+    wrapper.setProps({
+      role: TopicTypes.collection,
+      symbolKind: 'module',
+    });
+    const hero = wrapper.find(DocumentationHero);
+    expect(hero.props()).toEqual({ type: TopicTypes.module, enhanceBackground: true });
+  });
+
+  it('render a `DocumentationHero`, disabled, if symbol page', () => {
+    wrapper.setProps({
+      symbolKind: 'protocol',
+    });
+    const hero = wrapper.find(DocumentationHero);
+    expect(hero.props()).toEqual({ type: TopicTypes.protocol, enhanceBackground: false });
+  });
+
   it('renders a `Title`', () => {
-    const title = wrapper.find(Title);
+    const hero = wrapper.find(DocumentationHero);
+    const title = hero.find(Title);
     expect(title.exists()).toBe(true);
     expect(title.props('eyebrow')).toBe(propsData.roleHeading);
     expect(title.text()).toBe(propsData.title);
   });
 
+  it('renders smaller "Beta" and "Deprecated" text in title when relevant', () => {
+    const title = wrapper.find(Title);
+    expect(title.exists()).toBe(true);
+    let smalls = title.findAll('small');
+    expect(smalls.length).toBe(0);
+
+    // both beta _and_ deprecated — deprecated has priority
+    wrapper.setProps({
+      isSymbolDeprecated: true,
+      isSymbolBeta: true,
+    });
+    smalls = title.findAll('small');
+    expect(smalls.length).toBe(1);
+    expect(smalls.at(0).is('.beta')).toBe(false);
+    expect(smalls.at(0).is('.deprecated')).toBe(true);
+    expect(smalls.at(0).text()).toBe('Deprecated');
+
+    // only beta
+    wrapper.setProps({
+      isSymbolDeprecated: false,
+      isSymbolBeta: true,
+    });
+    smalls = title.findAll('small');
+    expect(smalls.length).toBe(1);
+    expect(smalls.at(0).is('.beta')).toBe(true);
+    expect(smalls.at(0).is('.deprecated')).toBe(false);
+    expect(smalls.at(0).text()).toBe('Beta');
+
+    // only deprecated
+    wrapper.setProps({
+      isSymbolDeprecated: true,
+      isSymbolBeta: false,
+    });
+    smalls = title.findAll('small');
+    expect(smalls.length).toBe(1);
+    expect(smalls.at(0).is('.beta')).toBe(false);
+    expect(smalls.at(0).is('.deprecated')).toBe(true);
+    expect(smalls.at(0).text()).toBe('Deprecated');
+  });
+
   it('renders an abstract', () => {
-    const descr = wrapper.find(Description);
-    const abstractComponent = descr.find(Abstract);
+    const hero = wrapper.find(DocumentationHero);
+    const abstractComponent = hero.find(Abstract);
     expect(abstractComponent.exists()).toBe(true);
     expect(abstractComponent.props('content')).toEqual(propsData.abstract);
   });
@@ -231,23 +308,10 @@ describe('DocumentationTopic', () => {
     wrapper.setProps({
       abstract: emptyParagraph,
     });
-    const descr = wrapper.find(Description);
-    const abstractComponent = descr.find(Abstract);
+    const hero = wrapper.find(DocumentationHero);
+    const abstractComponent = hero.find(Abstract);
     expect(abstractComponent.exists()).toBe(true);
     expect(abstractComponent.props('content')).toEqual(emptyParagraph);
-  });
-
-  it('renders a `.content-grid` with `Description`/`Summary and PrimaryContent` columns', () => {
-    const grid = wrapper.find('.content-grid.container');
-    expect(grid.exists()).toBe(true);
-
-    const description = grid.find(Description);
-    expect(description.exists()).toBe(true);
-
-    const summary = grid.find(Summary);
-    expect(summary.exists()).toBe(true);
-
-    expect(grid.find(PrimaryContent).exists()).toBe(true);
   });
 
   it('renders a `PrimaryContent`', () => {
@@ -305,96 +369,23 @@ describe('DocumentationTopic', () => {
       wrapper.setProps({ isRequirement: true });
       expect(wrapper.contains(RequirementMetadata)).toBe(true);
     });
-  });
-
-  describe('summary column', () => {
-    let summary;
-
-    beforeEach(() => {
-      summary = wrapper.find('main .container').find(Summary);
-    });
-
-    it('hides the Summary, if the global settings say so', () => {
-      // this should really only mock the resolved value for the specific flag,
-      // but this is fine for now
-      getSetting.mockResolvedValueOnce(true);
-      wrapper = shallowMount(DocumentationTopic, { propsData });
-      expect(wrapper.find(Summary).exists()).toBe(false);
-    });
 
     it('renders a `Availability` with platforms data', () => {
-      const platforms = [
-        {
-          introducedAt: '1.0',
-          name: 'fooOS',
-        },
-        {
-          deprecatedAt: '2.0',
-          introducedAt: '1.0',
-          name: 'barOS',
-        },
-      ];
-      wrapper.setProps({ platforms });
-
-      const list = summary.find(Availability);
+      const list = wrapper.find(Availability);
       expect(list.exists()).toBe(true);
-      expect(list.props('platforms')).toEqual(platforms);
+      expect(list.props('platforms')).toEqual(propsData.platforms);
     });
+  });
 
-    it('renders a `TechnologyList` with technologies data', () => {
-      const modules = ['FooKit', 'BarKit'];
-      wrapper.setProps({ modules });
-
-      const list = summary.find(TechnologyList);
-      expect(list.exists()).toBe(true);
-      expect(list.props('technologies')).toEqual(modules);
-    });
-
-    it('renders an `OnThisPageNav` with more than 1 section', () => {
-      const onThisPageSections = [
-        { anchor: 'foo', title: 'Foo' },
-        { anchor: 'bar', title: 'Bar' },
-      ];
-      wrapper.setData({ topicState: { onThisPageSections } });
-
-      const nav = summary.find(OnThisPageNav);
-      expect(nav.exists()).toBe(true);
-      expect(nav.props('sections')).toEqual(onThisPageSections);
-    });
-
-    it('does not render `OnThisPage` with 1 or fewer sections', () => {
-      const onThisPageSections = [{ anchor: 'foo', title: 'Foo' }];
-      wrapper.setData({ topicState: { onThisPageSections } });
-      expect(summary.contains(OnThisPageNav)).toBe(false);
-
-      wrapper.setData({ topicState: { onThisPageSections: [] } });
-      expect(summary.contains(OnThisPageNav)).toBe(false);
-    });
-
-    it('renders a `LanguageSwitcher`', () => {
-      const switcher = summary.find(LanguageSwitcher);
-      expect(switcher.exists()).toBe(true);
-      expect(switcher.props()).toEqual({
-        interfaceLanguage: propsData.interfaceLanguage,
-        objcPath: propsData.languagePaths.occ[0],
-        swiftPath: propsData.languagePaths.swift[0],
-      });
-    });
-
-    it('renders an `TechnologyList` component in the sidebar', () => {
-      expect(wrapper.find('.extends-technology').exists()).toBe(false);
-      const extendsTechnology = 'FooTechnology';
-
-      wrapper.setProps({
-        extendsTechnology,
-      });
-
-      const technologyList = wrapper.find('.extends-technology');
-      expect(technologyList.exists()).toBe(true);
-      expect(technologyList.props()).toEqual({
-        technologies: [{ name: extendsTechnology }],
-        title: 'Extends',
-      });
+  it('renders a `LanguageSwitcher` if TargetIDE', () => {
+    const provide = { isTargetIDE: true };
+    wrapper = shallowMount(DocumentationTopic, { propsData, provide });
+    const switcher = wrapper.find(LanguageSwitcher);
+    expect(switcher.exists()).toBe(true);
+    expect(switcher.props()).toEqual({
+      interfaceLanguage: propsData.interfaceLanguage,
+      objcPath: propsData.languagePaths.occ[0],
+      swiftPath: propsData.languagePaths.swift[0],
     });
   });
 
@@ -565,10 +556,10 @@ describe('DocumentationTopic', () => {
     wrapper = shallowMount(DocumentationTopic, {
       propsData,
       slots: {
-        'above-title': 'Above Title Content',
+        'above-title': '<div class="above-title">Above Title Content</div>',
       },
     });
-    expect(wrapper.text()).toContain('Above Title Content');
+    expect(wrapper.find(DocumentationHero).contains('.above-title')).toBe(true);
   });
 
   describe('lifecycle hooks', () => {

src/components/DocumentationTopic/Summary/Title.vue
@@ -1,7 +1,7 @@
 <!--
   This source file is part of the Swift.org open source project
 
-  Copyright (c) 2021 Apple Inc. and the Swift project authors
+  Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
   Licensed under Apache License v2.0 with Runtime Library Exception
 
   See https://swift.org/LICENSE.txt for license information
@@ -20,10 +20,13 @@ export default { name: 'Title' };
 @import 'docc-render/styles/_core.scss';
 
 .title {
-  color: var(--colors-text, var(--color-text));
-  @include font-styles(label-reduced);
+  color: dark-color(figure-gray);
   font-size: rem(14px);
-  margin-bottom: 0.5rem;
+  margin-right: 0.5rem;
   text-rendering: optimizeLegibility;
+
+  .documentation-hero--disabled & {
+    color: var(--colors-text, var(--color-text));
+  }
 }
 </style>

src/views/DocumentationTopic.vue
@@ -1,7 +1,7 @@
 <!--
   This source file is part of the Swift.org open source project
 
-  Copyright (c) 2021 Apple Inc. and the Swift project authors
+  Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
   Licensed under Apache License v2.0 with Runtime Library Exception
 
   See https://swift.org/LICENSE.txt for license information
@@ -145,7 +145,6 @@ export default {
           url: identifier,
         },
         metadata: {
-          extends: extendsTechnology,
           conformance,
           modules,
           platforms,
@@ -187,7 +186,6 @@ export default {
         topicSections,
         seeAlsoSections,
         variantOverrides,
-        extendsTechnology,
         symbolKind,
         tags: tags.slice(0, 1), // make sure we only show the first tag
       };

tests/unit/views/DocumentationTopic.spec.js
@@ -1,7 +1,7 @@
 /**
  * This source file is part of the Swift.org open source project
  *
- * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
  * Licensed under Apache License v2.0 with Runtime Library Exception
  *
  * See https://swift.org/LICENSE.txt for license information
@@ -338,6 +338,9 @@ describe('DocumentationTopic', () => {
       isSymbolBeta: false,
       isSymbolDeprecated: false,
       isWideFormat: false,
+      interfaceLanguage: topicData.identifier.interfaceLanguage,
+      objcPath: topicData.variants[0].paths[0],
+      swiftPath: topicData.variants[1].paths[0],
     });
     expect(nav.attributes()).toMatchObject({
       interfacelanguage: 'swift',

src/components/Badge.vue
@@ -1,7 +1,7 @@
 <!--
   This source file is part of the Swift.org open source project
 
-  Copyright (c) 2021 Apple Inc. and the Swift project authors
+  Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
   Licensed under Apache License v2.0 with Runtime Library Exception
 
   See https://swift.org/LICENSE.txt for license information
@@ -47,7 +47,7 @@ $badge-border-radius: $border-radius - 1px !default;
 
 @mixin badge-variation($color) {
   --badge-color: var(--color-badge-#{$color});
-  --badge-dark-color: var(--color-badge-dark-#{$color})
+  --badge-dark-color: var(--color-badge-dark-#{$color});
 }
 
 .badge {
@@ -67,6 +67,7 @@ $badge-border-radius: $border-radius - 1px !default;
     --badge-color: var(--badge-dark-color);
   }
 
+  /* nav bar badge */
   &-deprecated {
     @include badge-variation(deprecated);
   }

src/components/DocumentationTopic/ContentTable.vue
@@ -1,7 +1,7 @@
 <!--
   This source file is part of the Swift.org open source project
 
-  Copyright (c) 2021 Apple Inc. and the Swift project authors
+  Copyright (c) 2021-2022 Apple Inc. and the Swift project authors
   Licensed under Apache License v2.0 with Runtime Library Exception
 
   See https://swift.org/LICENSE.txt for license information
@@ -47,14 +47,18 @@ export default {
   @include inTargetIde {
     background: var(--color-content-table-content-color);
   }
-  padding: $section-spacing-single-side 0;
 }
 
 .container {
   @include dynamic-content-container;
+  padding-bottom: $section-spacing-single-side;
 }
 
 .title {
   @include font-styles(heading-2-reduced);
+  padding-top: $section-spacing-single-side;
+  border-top-color: var(--color-grid);
+  border-top-style: solid;
+  border-top-width: 1px;
 }
 </style>

commit b87395d5125841870664b2fd1e1a969fd05637a3

Fix bug where long titles are now broken at arbitrary characters.
This fixes a regression introduced on main with
f12b885045341728ebf94329f0581747a933753e as part of the updated layout
changes in support of the new navigator UX. (rdar://90170811)

src/components/DocumentationTopic.vue
@@ -27,7 +27,8 @@
           :swiftPath="swiftPath"
         />
         <Title :eyebrow="roleHeading">
-          {{ title }}
+          <WordBreak>{{ title }}</WordBreak>
+          <template v-if="isSymbolBeta || isSymbolDeprecated">&nbsp;</template>
           <small v-if="isSymbolDeprecated" slot="after" class="deprecated">Deprecated</small>
           <small v-else-if="isSymbolBeta" slot="after" class="beta">Beta</small>
         </Title>
@@ -95,6 +96,7 @@ import Aside from 'docc-render/components/ContentNode/Aside.vue';
 import BetaLegalText from 'theme/components/DocumentationTopic/BetaLegalText.vue';
 import LanguageSwitcher from 'theme/components/DocumentationTopic/Summary/LanguageSwitcher.vue';
 import DocumentationHero from 'docc-render/components/DocumentationTopic/DocumentationHero.vue';
+import WordBreak from 'docc-render/components/WordBreak.vue';
 import Abstract from './DocumentationTopic/Description/Abstract.vue';
 import ContentNode from './DocumentationTopic/ContentNode.vue';
 import CallToActionButton from './CallToActionButton.vue';
@@ -141,6 +143,7 @@ export default {
     SeeAlso,
     Title,
     Topics,
+    WordBreak,
   },
   props: {
     abstract: {

src/components/DocumentationTopic/Title.vue
@@ -11,19 +11,16 @@
 <template>
   <div class="topictitle">
     <span v-if="eyebrow" class="eyebrow">{{eyebrow}}</span>
-    <WordBreak class="title" tag="h1">
+    <h1 class="title">
       <slot />
       <slot name="after" />
-    </WordBreak>
+    </h1>
   </div>
 </template>
 
 <script>
-import WordBreak from 'docc-render/components/WordBreak.vue';
-
 export default {
   name: 'Title',
-  components: { WordBreak },
   props: {
     eyebrow: {
       type: String,

tests/unit/components/DocumentationTopic.spec.js
@@ -29,6 +29,7 @@ const {
   Topics,
   Title,
   BetaLegalText,
+  WordBreak,
 } = DocumentationTopic.components;
 
 const foo = {
@@ -259,10 +260,14 @@ describe('DocumentationTopic', () => {
 
   it('renders a `Title`', () => {
     const hero = wrapper.find(DocumentationHero);
+
     const title = hero.find(Title);
     expect(title.exists()).toBe(true);
     expect(title.props('eyebrow')).toBe(propsData.roleHeading);
-    expect(title.text()).toBe(propsData.title);
+
+    const wb = title.find(WordBreak);
+    expect(wb.exists()).toBe(true);
+    expect(wb.text()).toBe(propsData.title);
   });
 
   it('renders smaller "Beta" and "Deprecated" text in title when relevant', () => {
@@ -276,6 +281,7 @@ describe('DocumentationTopic', () => {
       isSymbolDeprecated: true,
       isSymbolBeta: true,
     });
+    expect(title.text()).toEqual(expect.stringMatching(/FooKit\s+Deprecated/));
     smalls = title.findAll('small');
     expect(smalls.length).toBe(1);
     expect(smalls.at(0).is('.beta')).toBe(false);
@@ -287,6 +293,7 @@ describe('DocumentationTopic', () => {
       isSymbolDeprecated: false,
       isSymbolBeta: true,
     });
+    expect(title.text()).toEqual(expect.stringMatching(/FooKit\s+Beta/));
     smalls = title.findAll('small');
     expect(smalls.length).toBe(1);
     expect(smalls.at(0).is('.beta')).toBe(true);
@@ -298,6 +305,7 @@ describe('DocumentationTopic', () => {
       isSymbolDeprecated: true,
       isSymbolBeta: false,
     });
+    expect(title.text()).toEqual(expect.stringMatching(/FooKit\s+Deprecated/));
     smalls = title.findAll('small');
     expect(smalls.length).toBe(1);
     expect(smalls.at(0).is('.beta')).toBe(false);

tests/unit/components/DocumentationTopic/Title.spec.js
@@ -36,11 +36,9 @@ describe('Title', () => {
     expect(eyebrow.text()).toBe('Thing');
   });
 
-  it('renders a `WordBreak` with an <h1> tag for the default slot', () => {
-    const { WordBreak } = Title.components;
-    const wb = wrapper.find(WordBreak);
-    expect(wb.classes('title')).toBe(true);
-    expect(wb.attributes('tag')).toBe('h1');
-    expect(wb.text()).toBe('FooKit');
+  it('renders <h1> tag for the default slot', () => {
+    const h1 = wrapper.find('h1');
+    expect(h1.classes('title')).toBe(true);
+    expect(h1.text()).toBe('FooKit');
   });
 });