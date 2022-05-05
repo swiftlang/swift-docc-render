4f16a2a0160f6e5a00c5faa1a35d8c701fc23df7
Hanqing Huang <87735557+hqhhuang@users.noreply.github.com>
2022-05-05 13:52:53 -0400

    Block display with change highlight 92325863 (#255)
    Block display with change highlight 92325863

--- src/components/DocumentationTopic/RelationshipsList.vue
+++ src/components/DocumentationTopic/RelationshipsList.vue
@@ -156,6 +156,11 @@ export default {
     &:after {
       margin-top: $change-coin-y-offset-reduced;
     }
+
+     // ensure that column layout stays a block content
+    &.column {
+      display: block;
+    }
   }
 }
 
