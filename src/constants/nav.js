817aae3e597fae71c52c8b6b3366ce9759c57390
Marcus Ortiz <marcus_ortiz@apple.com>
2022-05-10 16:30:54 -0700

    Allow non-menu-closing tray links (#284)
    Fixes: rdar://92984703

--- src/constants/nav.js
+++ src/constants/nav.js
@@ -8,7 +8,10 @@
  * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
 */
 
-// eslint-disable-next-line import/prefer-default-export
 export const baseNavHeight = 52;
 export const baseNavHeightSmallBreakpoint = 48;
 export const baseNavStickyAnchorId = 'nav-sticky-anchor';
+
+export const MenuLinkModifierClasses = {
+  noClose: 'noclose',
+};
