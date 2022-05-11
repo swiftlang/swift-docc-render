41625b9fc1ea4ccfd8242192dc6d651f6ff30f87
Marcus Ortiz <marcus_ortiz@apple.com>
2022-05-11 10:23:37 -0700

    Allow non-menu-closing tray links (#284) (#290)
    Fixes: rdar://92984703

--- tests/unit/components/NavBase.spec.js
+++ tests/unit/components/NavBase.spec.js
@@ -15,7 +15,7 @@ import BreakpointEmitter from 'docc-render/components/BreakpointEmitter.vue';
 import scrollLock from 'docc-render/utils/scroll-lock';
 import FocusTrap from 'docc-render/utils/FocusTrap';
 import changeElementVOVisibility from 'docc-render/utils/changeElementVOVisibility';
-import { baseNavStickyAnchorId } from 'docc-render/constants/nav';
+import { baseNavStickyAnchorId, MenuLinkModifierClasses } from 'docc-render/constants/nav';
 import { waitFrames } from 'docc-render/utils/loading';
 import { createEvent } from '../../../test-utils';
 
@@ -225,6 +225,21 @@ describe('NavBase', () => {
     expect(wrapper.classes()).toContain(NavStateClasses.isOpen);
     tray.find('.with-anchor a').trigger('click');
     expect(wrapper.classes()).not.toContain(NavStateClasses.isOpen);
+  });
+
+  it('does not close the navigation if clicked on a .noclose link inside the tray', async () => {
+    const { noClose } = MenuLinkModifierClasses;
+    wrapper = await createWrapper({
+      data: () => ({ isOpen: true }),
+      slots: {
+        'menu-items': `
+          <li class="with-anchor"><a class="${noClose}" href="#">Somewhere</a></li>
+          <li class="without-anchor"><div class="foo">Foo</div></li>`,
+      },
+    });
+    const tray = wrapper.find(NavMenuItems);
+    tray.find('.with-anchor a').trigger('click');
+    expect(wrapper.classes()).toContain(NavStateClasses.isOpen);
   });
 
   it('adds extra classes to stop scrolling while animating the tray up/down', async () => {
