2db9e23d0cd7eb685d1407b288e42ba0c89a58bc
Marina Aísa <marina.aisa@apple.com>
2022-08-03 17:24:34 +0200

    [AX] Color scheme toggle keyboard navigation should follow radio buttons standards (#401)
    * [r94221573] fix: color scheme toggle should focus as a radiobutton group
    
    * [r94221573] fix: small typo

--- src/components/ColorSchemeToggle.vue
+++ src/components/ColorSchemeToggle.vue
@@ -13,7 +13,6 @@
     aria-label="Select a color scheme preference"
     class="color-scheme-toggle"
     role="radiogroup"
-    tabindex="0"
   >
     <label
       v-for="option in options"
@@ -83,6 +82,12 @@ export default {
 input {
   @include visuallyhidden;
   appearance: none;
+}
+
+label {
+  @include on-keyboard-focus-within() {
+    @include focus-outline();
+  }
 }
 
 .text {
