d286b1e34b3f1c0b37299b0bb1906e3bf468b18b
Dobromir Hristov <dhristov@apple.com>
2022-09-20 14:10:26 +0300

    refactor: add extra offset when scrolling to a section in Documentation pages

---
+++ src/utils/scroll-offset.js
@@ -0,0 +1,23 @@
+/**
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2021 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
+import { documentationTopicName } from 'docc-render/constants/router';
+
+export const EXTRA_DOCUMENTATION_OFFSET = 10;
+
+/**
+ * @param {import('vue-router').Route} to
+ * @returns {Number}
+ */
+export default function getExtraScrollOffset(to) {
+  const { name } = to;
+  const isDocumentation = name.includes(documentationTopicName);
+  return isDocumentation ? EXTRA_DOCUMENTATION_OFFSET : 0;
+}
