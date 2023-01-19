ddf6b709798bf1a426b7e722f65ee3d55aa96200
Marina Aísa <marina.aisa@apple.com>
2023-01-19 15:12:42 +0100

    [r102139999] fix: license headers

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,3 +1,13 @@
+/**
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2023 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
+
 export { default as en } from './en_US.json';
 export { default as zh } from './zh_CN.json';
 export { default as ja } from './ja_JA.json';
