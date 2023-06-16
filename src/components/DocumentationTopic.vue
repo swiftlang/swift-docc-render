929ee3578d1b20e7ff0768fb2ded111e49ace746
Dobromir Hristov <dhristov@apple.com>
2023-06-16 18:21:54 +0300

    Move the references setting back to the DocTopic component (#696)
    resolves rdar://109856608
    
    * refactor: move the references setting
    * fix: update references in store, if they update and add tests
    * refactor: add watchers to the references on top Component type pages

--- src/components/DocumentationTopic.vue
+++ src/components/DocumentationTopic.vue
@@ -622,6 +622,13 @@ export default {
     }
 
     this.store.reset();
+    this.store.setReferences(this.references);
+  },
+  watch: {
+    // update the references in the store, in case they update, but the component is not re-created
+    references(references) {
+      this.store.setReferences(references);
+    },
   },
 };
 </script>
