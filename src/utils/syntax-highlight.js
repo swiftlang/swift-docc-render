swiftlang/swift-docc-render

commit 6f9f6388b71fd99916a2fcf4655665ba87a69ab7

Ensure highlight.js builds only the files that are imported (#724)
rdar://113252221

src/utils/syntax-highlight.js
@@ -90,7 +90,7 @@ async function importHighlightFileForLanguage(language) {
         languageFile = await import(
           // See bug https://github.com/webpack/webpack/issues/13865
           /* webpackChunkName: "highlight-js-[request]" */
-          `@/../node_modules/highlight.js/lib/languages/${file}`
+          `highlight-js-alias/lib/languages/${file}`
         );
       }
 

jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
   moduleNameMapper: {
     '^docc-render/(.*)$': '<rootDir>/src/$1',
     '^theme/(.*)$': '<rootDir>/src/$1',
+    '^highlight-js-alias/(.*)$': '<rootDir>/node_modules/highlight.js/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
   setupFiles: ['./tests/unit/config.js'],

src/setup-utils/vue-config-utils.js
@@ -53,6 +53,7 @@ function baseChainWebpack(config) {
   // Add theme fallback resolver
   config.resolve.alias.set('theme', themeUtils.getThemePaths());
   config.resolve.alias.set('docc-render', path.resolve(__dirname, '../'));
+  config.resolve.alias.set('highlight-js-alias', path.dirname(require.resolve('highlight.js/package.json')));
 
   // Add license header to built files
   config
@@ -68,7 +69,7 @@ function baseChainWebpack(config) {
     .plugin('LanguagesPlugin')
     .use(webpack.ContextReplacementPlugin, [
       /highlight\.js\/lib\/languages$/,
-      new RegExp(`/(${[builtinLanguages, envLanguages].join('|')})$`),
+      new RegExp(`^./(${[builtinLanguages, envLanguages].filter(Boolean).join('|')})\\.js$`),
     ]);
 }
 

commit 918407f073ab4c239e1991d122ac196fbfce9491

Fix broken import for non-custom syntax langs.
This is a regression mistakenly introduced with [#724][0] that prevents
non-custom syntax languages (everything except swift/markdown) from
being highlighted as expected due to an `import` call that is throwing
an exception.

To fix it, we can simply append the `.js` file extension to ensure that
the import continues to work as expected.

[0]: https://github.com/apple/swift-docc-render/pull/724

src/utils/syntax-highlight.js
@@ -90,7 +90,7 @@ async function importHighlightFileForLanguage(language) {
         languageFile = await import(
           // See bug https://github.com/webpack/webpack/issues/13865
           /* webpackChunkName: "highlight-js-[request]" */
-          `highlight-js-alias/lib/languages/${file}`
+          `highlight-js-alias/lib/languages/${file}.js`
         );
       }
 