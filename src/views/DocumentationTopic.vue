2d0c69878ff48d798aaac04db3c7083132edc4bb
Hanqing Huang <87735557+hqhhuang@users.noreply.github.com>
2023-11-27 10:17:13 -0500

    Match for generic item in the navigator (#756)
    * match generic item in navigator rdar://117442044

--- src/views/DocumentationTopic.vue
+++ src/views/DocumentationTopic.vue
@@ -43,6 +43,7 @@
                     :error-fetching="slotProps.errorFetching"
                     :api-changes="slotProps.apiChanges"
                     :references="topicProps.references"
+                    :symbolKind="topicProps.symbolKind"
                     :navigator-references="slotProps.references"
                     :scrollLockID="scrollLockID"
                     :render-filter-on-top="breakpoint !== BreakpointName.large"
