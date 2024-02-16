fe2a6f3127cd52908e484aa2affcf71b99597e10
Marcus Ortiz <marcus_ortiz@apple.com>
2024-02-15 16:22:57 -0800

    Move license comment to top of file.

--- src/components/Pager.vue
+++ src/components/Pager.vue
@@ -1,3 +1,12 @@
+/**
+ * This source file is part of the Swift.org open source project
+ *
+ * Copyright (c) 2022 Apple Inc. and the Swift project authors
+ * Licensed under Apache License v2.0 with Runtime Library Exception
+ *
+ * See https://swift.org/LICENSE.txt for license information
+ * See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+*/
 <script setup>
 import { ref } from 'vue';
 
@@ -48,15 +57,6 @@ function setActivePage(n) {
 export default { name: 'Pager' };
 </script>
 
-<!--
-  This source file is part of the Swift.org open source project
-
-  Copyright (c) 2024 Apple Inc. and the Swift project authors
-  Licensed under Apache License v2.0 with Runtime Library Exception
-
-  See https://swift.org/LICENSE.txt for license information
-  See https://swift.org/CONTRIBUTORS.txt for Swift project authors
--->
 <template>
   <div class="pager">
     <div
