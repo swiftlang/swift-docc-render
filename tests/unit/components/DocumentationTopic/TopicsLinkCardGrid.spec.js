e841161bde0f5b83f612ccc0aa34e14b72354044
Marina Aísa <marina.aisa@apple.com>
2024-03-15 18:19:41 +0100

    Fix medium breakpoint comment

--- tests/unit/components/DocumentationTopic/TopicsLinkCardGrid.spec.js
+++ tests/unit/components/DocumentationTopic/TopicsLinkCardGrid.spec.js
@@ -56,7 +56,7 @@ describe('TopicsLinkCardGrid', () => {
       expect(pager.exists()).toBe(true);
       expect(pager.props('pages').length).toBe(2);
 
-      // 10 items => 2 pages at large breakpoint (6 links per page)
+      // 10 items => 2 pages at medium breakpoint (6 links per page)
       wrapper.setData({ breakpoint: BreakpointName.medium });
       pager = wrapper.find(Pager);
       expect(pager.exists()).toBe(true);
