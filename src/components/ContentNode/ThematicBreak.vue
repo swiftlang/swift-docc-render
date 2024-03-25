16c208b246b3981b972236306e99ea6cf9b1f552
Marcus Ortiz <marcus_ortiz@apple.com>
2024-03-25 12:18:05 -0700

    Add support for "thematic breaks" using `<hr>`.

---
+++ src/components/ContentNode/ThematicBreak.vue
@@ -0,0 +1,21 @@
+<!--
+  This source file is part of the Swift.org open source project
+
+  Copyright (c) 2024 Apple Inc. and the Swift project authors
+  Licensed under Apache License v2.0 with Runtime Library Exception
+
+  See https://swift.org/LICENSE.txt for license information
+  See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+-->
+<template>
+  <hr class="thematic-break" />
+</template>
+
+<style scoped lang="scss">
+@import 'docc-render/styles/_core.scss';
+
+.thematic-break {
+  @include space-out-between-siblings(var(--spacing-stacked-margin-xlarge));
+  border-color: var(--color-grid, currentColor);
+}
+</style>
