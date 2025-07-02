11b956fb8d1d58940bcc15b4e9b5c6c74e22c09a
Marina Aísa <marina.aisa@apple.com>
2025-07-02 15:56:46 +0100

    Update Node.js & NPM in project's engines
    This change updates Node.js to 22.17.0 & NPM to 10.9.2. I chose 22.17.0
    version of Node because it's the latest LTS (Long Term Support) version.

--- build-script-helper.py
+++ build-script-helper.py
@@ -75,8 +75,8 @@ def ensure_npm_is_installed(verbose=False):
         fatal_error('-- Error: %s' % error_msg)
     try:
         node_version = check_output(['node', '--version'], verbose=verbose)
-        if not node_version.startswith('v18.16.'):
-            warn_msg = "Unexpected version of 'node' installed. Swift-DocC-Render requires node 18.16.1. "\
+        if not node_version.startswith('v22.17.'):
+            warn_msg = "Unexpected version of 'node' installed. Swift-DocC-Render requires node 22.17.0. "\
                 "See the README.md file for more information about building Swift-DocC-Render."
             printerr('-- Warning: %s' % warn_msg)
     except:
