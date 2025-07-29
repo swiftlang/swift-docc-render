fc6db82c858439df71fa735ed15782c86ec0d161
Jesse Haigh <jhaigh@apple.com>
2025-07-29 17:12:40 -0600

    add copy-to-clipboard button on code listings with success/failure UI
    - Show copy-to-clipboard button on hover for devices that support it
    - Persistently display button on non-hover devices
    - Add copyCodeToClipboard function to write code block text to clipboard
    - Add Copy/Checkmark/Cross icons with CopyState for state-specific styling

---
+++ src/components/Icons/CheckmarkIcon.vue
@@ -0,0 +1,35 @@
+<!--
+  This source file is part of the Swift.org open source project
+
+  Copyright (c) 2025 Apple Inc. and the Swift project authors
+  Licensed under Apache License v2.0 with Runtime Library Exception
+
+  See https://swift.org/LICENSE.txt for license information
+  See https://swift.org/CONTRIBUTORS.txt for Swift project authors
+-->
+
+<template>
+  <SVGIcon
+    class="CheckmarkIcon"
+    viewBox="0 0 24 24"
+    themeId="checkmark"
+  >
+    <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/>
+  </SVGIcon>
+</template>
+
+<script>
+import SVGIcon from 'docc-render/components/SVGIcon.vue';
+
+export default {
+  name: 'CheckmarkIcon',
+  components: { SVGIcon },
+};
+</script>
+
+<style scoped lang="scss">
+.CheckmarkIcon {
+  opacity: 1;
+  stroke: currentColor;
+}
+</style>
