b5f8aeb598598b80ed2b1d6b7fea859532ac1dd7
Marcus Ortiz <marcus_ortiz@apple.com>
2026-04-01 11:28:37 -0700

    Update base card styling.

--- src/components/ContentNode/Card.vue
+++ src/components/ContentNode/Card.vue
@@ -16,8 +16,19 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import 'docc-render/styles/_core.scss';
+
 .card {
-  background: var(--color-fill-tertiary);
+  --card-border-radius: 4px;
+  --card-border-style: solid;
+  --card-border-width: 1px;
+  --card-color-border: var(--color-fill);
+  --card-color-content: var(--color-fill-secondary);
+  --card-color-head: var(--color-fill-tertiary);
+
+  background: var(--card-color-content);
+  border: var(--card-border-width) var(--card-border-style) var(--card-color-border);
+  border-radius: var(--card-border-radius);
 
   & + *,
   * + & {
@@ -29,7 +40,9 @@ export default {
   }
 
   &-head {
-    background: var(--color-fill-secondary);
+    background: var(--card-color-head);
+    border-start-start-radius: inherit;
+    border-start-end-radius: inherit;
   }
 }
 </style>
